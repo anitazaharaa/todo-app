f3ff06b21a2383f39050f6810b17be7ca6209f8e
anitazaharaa <anitazaharaa@gmail.com>
2025-10-17 13:28:26 +0700

    delete file

--- src/types/task.ts
+++
@@ -1,8 +0,0 @@
-export interface Task {
-  id: number;               
-  title: string;            
-  description: string;      
-  completed: boolean;
-  created_at: string;      
-  updated_at: string;
-}
